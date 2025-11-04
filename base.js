98f7d4c368f8a12e5bc45c82e561a55359d845c2
victorrodegherosantos-coder <victor.rodeghero.santos@escola.pr.gov.br>
2025-11-03 21:45:42 -0300

    Update base.js

--- base.js
+++ base.js
@@ -24,8 +24,49 @@ titlebs.forEach(el=>{
   
 el.addEventListener("click",()=>
 {
-window.location.href=""
+window.location.href="";
 })
   
 
 })
+var vibras=[];
+var vibraquant=0;
+const seletor=document.getElementById("seletorarquivob");
+const stat=document.getElementById("status");
+var vb;
+seletor.addEventListener('change',function(e){
+const arq=e.target.files[0];
+if(!arq){
+stat.innerText="Arquivo Falho."
+}
+
+const reader=new FileReader();
+
+reader.onload=function(ev)
+{
+const conteudo=ev.target.result;
+if(conteudo.slice(0,5)!="vibra")
+{
+stat.innerText="Arquivo Incorreto.";
+}else{
+stat.innerText="Sucesso!";
+vibraquant=conteudo.charCodeAt(5);
+vb=conteudo.slice(6,6+vibraquant*4);
+for(let i=0;i<vibraquant;i++)
+{
+vibras[i]=parseInt(vb.slice(i*4,i*4+4));
+console.log(vb.slice(i*4,i*4+4));
+	
+}
+console.log(conteudo);
+
+}
+};
+
+reader.readAsText(arq,'utf8');
+	
+});
+
+document.getElementById("tocarvib").addEventListener("click",()=>{
+navigator.vibrate(vibras);
+});
